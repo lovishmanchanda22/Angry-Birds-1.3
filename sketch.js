3cc47856c389836b8be7c69cf6859e3c30cfa35b
lovishmanchanda22 <meenumanchanda82@gmail.com>
2020-10-02 15:45:35 +0530

    version 1.3

--- sketch.js
+++ sketch.js
@@ -1,10 +1,13 @@
 const Engine = Matter.Engine;
 const World= Matter.World;
 const Bodies = Matter.Bodies;
+const Constraint = Matter.Constraint;
 
 var engine, world;
 var box1, pig1;
 var backgroundImg,platform;
+var log6;
+var chain;
 
 function preload() {
     backgroundImg = loadImage("sprites/bg.png");
@@ -14,8 +17,7 @@ function setup(){
     var canvas = createCanvas(1200,400);
     engine = Engine.create();
     world = engine.world;
-
-
+    
     ground = new Ground(600,height,1200,20);
     platform = new Ground(150, 305, 300, 170);
 
@@ -35,6 +37,10 @@ function setup(){
     log5 = new Log(870,120,150, -PI/7);
 
     bird = new Bird(100,100);
+
+    log6=new Log(230,180,80,PI/2);
+
+    chain = new Chain(bird.body, log6.body);
 
 }
 
@@ -61,4 +67,7 @@ function draw(){
 
     bird.display();
     platform.display();
+    log6.display();
+
+   chain.display();
 }
